f78a0c57947a46939a59b2b9434077184f9c228d
pyksoft <pyksoft@gmail.com>
2015-02-04 16:07:29 -0600

    Setting up environment

---
+++ UiUnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace UiUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
